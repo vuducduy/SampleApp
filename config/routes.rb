aa591001be4271b154605b2aede2ec4bc215a599
s2snowflakes <tuannguyenk60@gmail.com>
2017-09-25 21:09:11 +0700

    temp

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get  "help"		=>	"static_pages#help"
   get  "about"		=>	"static_pages#about"
   get  "contact"	=>	"static_pages#contact"
+  get "/signup" => "users#new"
   root "static_pages#home"
-
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
